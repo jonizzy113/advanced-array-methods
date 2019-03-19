c1fd6d50eb2e4d1d734a0aaa08b3a3b58cee73c7
Jonathan Schaffer <jonizzyschaffer@gmail.com>
2019-03-19 15:04:24 -0500

    Added filter method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,41 @@ outEl.innerHTML = "<h1>Active Businesses</h1>"
 
 let zipCodeProperty = "addressZipCode"
 
-businesses.forEach(business => {
+// businesses.forEach(business => {
+//     outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//     ${business.addressCity}, ${business["addressStateCode"]}, ${business[zipCodeProperty]}
+//     </section>
+//   `
+//     outEl.innerHTML += "<hr/>"
+// });
+
+
+// Array to contain all the New York businesses
+const newYorkBusinesses = businesses.filter(business => {
+    let inNewYork = false
+    if (business.addressStateCode === "NY") {
+        inNewYork = true
+    }
+    return inNewYork
+  })
+
+  const manufacturingBusinesses = businesses.filter(business => {
+      if (business.companyIndustry === "Manufacturing") {
+          return true
+      } else {
+          return false
+      }
+      
+  })
+
+  console.log(manufacturingBusinesses)
+
+  manufacturingBusinesses.forEach(business => {
     outEl.innerHTML += `
     <h2>${business.companyName}</h2>
     <section>
