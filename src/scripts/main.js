a749bd44c81e8ee817cc0da4e920aa36961ad987
Jonathan Schaffer <jonizzyschaffer@gmail.com>
2019-03-20 11:17:32 -0500

    Reduce method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -121,29 +121,72 @@ let zipCodeProperty = "addressZipCode"
 //         }
 //     });
 
-document
-    .querySelector("#companySearch")
-    .addEventListener("keypress", keyPressEvent => {
-        if (keyPressEvent.charCode === 13) {
-            /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
-            const foundBusiness = businesses.find(
-                business =>
-                    business.purchasingAgent.nameFirst.includes(keyPressEvent.target.value) ||business.purchasingAgent.nameLast.includes(keyPressEvent.target.value)
-            );
+// document
+//     .querySelector("#companySearch")
+//     .addEventListener("keypress", keyPressEvent => {
+//         if (keyPressEvent.charCode === 13) {
+//             /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+//             const foundBusiness = businesses.find(
+//                 business =>
+//                     business.purchasingAgent.nameFirst.includes(keyPressEvent.target.value) ||business.purchasingAgent.nameLast.includes(keyPressEvent.target.value)
+//             );
 
-            outEl.innerHTML = `
-                <h2>
-                ${foundBusiness.purchasingAgent.nameFirst +   " "+foundBusiness.purchasingAgent.nameLast}
-                </h2>
-                <section>
-                ${foundBusiness.addressFullStreet}
+//             outEl.innerHTML = `
+//                 <h2>
+//                 ${foundBusiness.purchasingAgent.nameFirst +   " "+foundBusiness.purchasingAgent.nameLast}
+//                 </h2>
+//                 <section>
+//                 ${foundBusiness.addressFullStreet}
 
-                </section>
-                <section>
-                ${foundBusiness.addressCity},
-                ${foundBusiness.addressStateCode}
-                ${foundBusiness.addressZipCode}
-                </section>
-            `;
-        }
-    });
+//                 </section>
+//                 <section>
+//                 ${foundBusiness.addressCity},
+//                 ${foundBusiness.addressStateCode}
+//                 ${foundBusiness.addressZipCode}
+//                 </section>
+//             `;
+//         }
+//     });
+
+
+// reduce method
+// businesses.forEach(business => {
+//     /* CALCULATE ORDER SUMMARY */
+// let totalOrders = business.orders.reduce(
+//     (currentTotal, nextValue) => currentTotal += nextValue,
+//     0
+// )
+
+//     outEl.innerHTML += `
+//         <h2>
+//             ${business.companyName}
+//             ($${totalOrders})
+//         </h2>
+//         <section>
+//             ${business.addressFullStreet}
+//         </section>
+//         <section>
+//             ${business.addressCity},
+//             ${business.addressStateCode}
+//             ${business.addressZipCode}
+//         </section>
+//     `;
+//     outEl.innerHTML += "<hr/>";
+// });
+
+
+// lighting exercise 1 
+const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
+
+const totalRainfall = monthlyRainfall.reduce((cuurentTotal, nextValue) =>
+cuurentTotal +=nextValue)
+
+
+console.log(totalRainfall)
+
+// lighting excersice 2 
+const words = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+const sentence = words.reduce((sentence, word) => sentence + " " + word)
+
+console.log(sentence)
